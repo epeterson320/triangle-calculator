04d039373570f2e35ab39ceb01c5e856a8b5b174
Eric Peterson <epeterson@sanfordguide.com>
2017-02-10 21:22:21 -0500

    Add React & Hot-Reloading

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "env": {
+        "browser": true,
+    },
+    "extends": "airbnb",
+    "plugins": [
+        "react",
+        "jsx-a11y",
+        "import"
+    ]
+};
--- src/index.js
+++
@@ -1,1 +0,0 @@
-// Todo: Import react, render in document
