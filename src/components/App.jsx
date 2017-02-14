d282c59cac70f7f0658f611296e0a5631326ac32
Eric Peterson <epeterson@sanfordguide.com>
2017-02-14 10:39:19 -0500

    Add notes to App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,8 +9,27 @@ const App = () => (
   </div>
 );
 /*
-Views (2 screens on mobile, 1 screen on tablet).
+On Mobile & portrait tablet
+Bottom navigation w/ 2 options: Edit, Results
+
+On landscape Tablet:
+Edit view on left, results on right
+
+On Web or widest screens:
+Input form on left, edit view in middle, results on right
+
+Views:
 Edit (Drag point, input boxes for measurements)
 Results (Scrolling text view showing work)
+
+Route:
+ericp.co/trig
+  Param     Value          Default
+  ?view     =edit|results // edit
+  &sides    =,60.5        // ,,
+  &angles   =30.1,,45     // ,,
+  &rotate   =2.2          // 0
+  &pan      =30.1,20.2    // 0,0
+  &showWork =false        // true
 */
 export default App;
