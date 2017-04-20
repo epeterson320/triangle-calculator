8acf40187b34c4d3dc2ea9adf55c3530e86be08c
Eric <epeterson320@gmail.com>
2017-04-20 17:08:43 -0400

    Add pending tests

--- src/components/MeasurementInput.test.jsx
+++ src/components/MeasurementInput.test.jsx
@@ -13,8 +13,21 @@ describe('<MeasurementInput>', () => {
       .toBe('ABC');
   });
 
+  it('Should match the label\'s "for" to the input\'s "id"');
+
   it('Should have a "clear" button', () => {
     expect(shallow(<MeasurementInput label="ABC" />).find('button').length)
       .toBe(1);
   });
+
+  it('Should accept an "onChange" property');
+  it('Should not call onChange immediately');
+  it('Should call onChange 300ms after a change');
+  it('Should delay/debounce the call to onChange if subsequent changes are made');
+  it('Should not show the "clear" button if the input is empty');
+
+  it('Should accept a "computed" property');
+  it('Should disable input if the value is computed');
+  it('Should not show the "clear" button if the value is computed');
+  it('Should call "onClear" when the clear button is clicked');
 });
