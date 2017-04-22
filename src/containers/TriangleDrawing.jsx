105989b2f56589cb3d8069cc12e30b9c85d4c0a9
Eric <epeterson320@gmail.com>
2017-04-22 16:39:12 -0400

    Add SVG element stub

--- src/containers/TriangleDrawing.jsx
+++ src/containers/TriangleDrawing.jsx
@@ -1,5 +1,15 @@
 import React from 'react';
 
-const TriangleDrawing = props => <code>Triangle\n{JSON.stringify(props, null, 2)}</code>;
+const TriangleDrawing = ({ triangle }) => {
+  return <code>{JSON.stringify(triangle, null, 2)}</code>;
+  /*
+  const { a, b, c } = triangle;
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg">
+      <path d={`M ${a.x},${a.y} L ${b.x},${b.y} L ${c.x},${c.y} Z`} />
+    </svg>
+  );
+  */
+};
 
 export default TriangleDrawing;
