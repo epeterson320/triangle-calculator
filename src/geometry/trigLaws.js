d14f16b935fabe837dd86a2645d453a579b80099
Eric <epeterson320@gmail.com>
2017-04-21 20:01:14 -0400

    Add tests for LoS and LoC trig stuff

--- src/geometry/trigLaws.js
+++ src/geometry/trigLaws.js
@@ -1,11 +1,14 @@
-export function fromSSAc() {
+export function fromSSAc(s1, s2, a3) {
+  const s3sq = (s1 * s1) + (s2 * s2) - (2 * s1 * s2 * Math.cos(a3));
+  const s3 = Math.sqrt(s3sq);
+  return s3;
 }
 
-export function fromSSS(s1, s2, s3) {
-  const a3 = Math.acos(((s1 * s1) + (s2 * s2) - (s3 * s3)) / (2 * s1 * s2));
-  const a2 = Math.asin(s2 * Math.sin(a3) / s3);
-  const a1 = Math.PI - a2 - a3;
-  return [a1, a2, a3];
+export function fromSSS(a, b, c) {
+  const C = Math.acos(((a * a) + (b * b) - (c * c)) / (2 * a * b));
+  const A = Math.asin(a * Math.sin(C) / c);
+  const B = Math.PI - A - C;
+  return [A, B, C];
 }
 
 export function fromSSAuc() {
--- src/geometry/trigLaws.test.js
+++ src/geometry/trigLaws.test.js
@@ -5,23 +5,41 @@ import {
   fromSAA,
 } from './trigLaws';
 
-const A_30 = Math.PI / 6;
-const A_60 = Math.PI / 3;
-const A_90 = Math.PI / 2;
+const C = Math.PI / 6;
+const B = Math.PI / 3;
+const A = Math.PI / 2;
+const a = 2;
+const b = Math.sqrt(3);
+const c = 1;
 
 describe('Law of cosines', () => {
   it('Finds the angles from 3 sides', () => {
-    const expected = [A_30, A_90, A_60];
-    const actual = fromSSS(1, 2, Math.sqrt(3));
+    const expected = [A, B, C];
+    const actual = fromSSS(a, b, c);
     actual.forEach((angle, i) => {
       expect(angle).toBeCloseTo(expected[i]);
     });
   });
 
-  it('Finds the third side from 2 sides with a common angle');
+  it('Finds the third side from 2 sides with a common angle', () => {
+    expect(fromSSAc(a, b, C)).toBeCloseTo(c);
+  });
 });
 
 describe('Law of sines', () => {
-  it('Finds the measurements from 2 sides with an uncommon angle');
-  it('Finds the measurements from 2-3 angles and any side');
+  it('Finds the measurements from 2 sides with an uncommon angle', () => {
+    const actual = fromSSAuc(a, b, A);
+    const expected = { a, b, c, A, B, C };
+    Object.keys(expected).forEach((key) => {
+      expect(actual[key]).toBeCloseTo(expected[key]);
+    });
+  });
+
+  it('Finds the measurements from 2-3 angles and any side', () => {
+    const actual = fromSAA(a, A, B);
+    const expected = { a, b, c, A, B, C };
+    Object.keys(expected).forEach((key) => {
+      expect(actual[key]).toBeCloseTo(expected[key]);
+    });
+  });
 });
